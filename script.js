9ecfbe5e71ccfe994c7fcd7272a7de01cc9357cb
Danny Chambers <connect@designtechnologist.co.uk>
2024-11-18 19:24:01 +0000

    Latest

--- script.js
+++ script.js
@@ -3,9 +3,9 @@ let systemName = "",
 
 const data = [
   {
-    name: "Elastic Web Components",
-    themes: ["brand-1"]
-  }
+    name: "{NAME OF YOUR SYSTEM}",
+    themes: ["brand-1"],
+  },
 ];
 
 const detectSystems = (data) => {
@@ -28,13 +28,15 @@ const detectSystems = (data) => {
   let uniqueDSComponentDetections = [...new Set(componentNames)]; // Ensure unique entries
 
   // Load or initialize local settings
-  if (localStorage.getItem("EWC-COMPONENT-TOOLS")) {
-    localDSSettings = JSON.parse(localStorage.getItem("EWC-COMPONENT-TOOLS"));
+  if (localStorage.getItem("COMPONENT-TOOLS")) {
+    localDSSettings = JSON.parse(localStorage.getItem("COMPONENT-TOOLS"));
   } else {
-    localDSSettings = JSON.parse(localStorage.setItem(
-      "EWC-COMPONENT-TOOLS",
-      JSON.stringify({ theme: "brand-1" })
-    ));
+    localDSSettings = JSON.parse(
+      localStorage.setItem(
+        "COMPONENT-TOOLS",
+        JSON.stringify({ theme: "brand-1" })
+      )
+    );
   }
 
   return {
@@ -42,7 +44,7 @@ const detectSystems = (data) => {
     nonSystemComponents,
     DSComponentDetections: componentNames,
     uniqueDSComponentDetections,
-    localDSSettings
+    localDSSettings,
   };
 };
 
@@ -58,36 +60,47 @@ const buildPopUpTools = (
       systemThemes = item.themes;
 
       // Update system detection information in the UI
-      document.getElementById("detected-system").innerHTML = `<em>${systemName}</em> was detected on this page.`;
-      document.getElementById("components-label").innerHTML = "Number of component instances:";
-      document.getElementById("components-value").innerHTML = `${DSComponentDetections.length}`;
-      document.getElementById("unique-components-label").innerHTML = "Number of unique components:";
-      document.getElementById("unique-components-value").innerHTML = `${uniqueDSComponentDetections.length}`;
+      document.getElementById(
+        "detected-system"
+      ).innerHTML = `<em>${systemName}</em> was detected on this page.`;
+      document.getElementById("components-label").innerHTML =
+        "Number of component instances:";
+      document.getElementById(
+        "components-value"
+      ).innerHTML = `${DSComponentDetections.length}`;
+      document.getElementById("unique-components-label").innerHTML =
+        "Number of unique components:";
+      document.getElementById(
+        "unique-components-value"
+      ).innerHTML = `${uniqueDSComponentDetections.length}`;
       document.getElementById("system-controls").style.display = "block";
 
       // Build theme options
       let themeOptions = "";
       systemThemes.forEach((theme, i) => {
-        themeOptions += `<option value="${theme}" ${i === 0 ? "selected" : ""}>${theme} (default)</option>`;
+        themeOptions += `<option value="${theme}" ${
+          i === 0 ? "selected" : ""
+        }>${theme} (default)</option>`;
       });
       document.getElementById("themes-select").innerHTML = themeOptions;
 
       // Build component options with 'None' as the first option
-      let componentOptions = "<option value=\"none\" selected>None</option>";
+      let componentOptions = '<option value="none" selected>None</option>';
       uniqueDSComponentDetections.forEach((component) => {
         componentOptions += `<option value="${component}">${component}</option>`;
       });
-      document.getElementById("highlight-components-select").innerHTML = componentOptions;
+      document.getElementById("highlight-components-select").innerHTML =
+        componentOptions;
     }
   });
 };
 
 const changeTheme = (selectedValue) => {
-  let localSettings = JSON.parse(localStorage.getItem("EWC-COMPONENT-TOOLS"));
+  let localSettings = JSON.parse(localStorage.getItem("COMPONENT-TOOLS"));
 
   localStorage.setItem("DSTheme", selectedValue);
   localStorage.setItem(
-    "EWC-COMPONENT-TOOLS",
+    "COMPONENT-TOOLS",
     JSON.stringify({ ...localSettings, theme: selectedValue })
   );
   window.dispatchEvent(new Event("storage"));
@@ -95,7 +108,7 @@ const changeTheme = (selectedValue) => {
 
 const highlightComponents = (selectedComponent) => {
   const removeExistingHighlights = () => {
-    document.querySelectorAll("span.highlight-wrapper").forEach(wrapper => {
+    document.querySelectorAll("span.highlight-wrapper").forEach((wrapper) => {
       while (wrapper.firstChild) {
         wrapper.parentNode.insertBefore(wrapper.firstChild, wrapper);
       }
@@ -113,7 +126,7 @@ const highlightComponents = (selectedComponent) => {
     wrapper.className = "highlight-wrapper";
     wrapper.style.border = "2px solid #7F00FF"; // Violet
 
-    if(getComputedStyle(element).display === "inline"){
+    if (getComputedStyle(element).display === "inline") {
       wrapper.style.display = "block";
     } else {
       wrapper.style.display = getComputedStyle(element).display;
@@ -125,15 +138,19 @@ const highlightComponents = (selectedComponent) => {
 
   removeExistingHighlights();
 
-  document.querySelectorAll(`[data-component="${selectedComponent}"]`).forEach(el => {
-    applyHighlight(el);
+  document
+    .querySelectorAll(`[data-component="${selectedComponent}"]`)
+    .forEach((el) => {
+      applyHighlight(el);
 
-    if (el.shadowRoot) {
-      el.shadowRoot.querySelectorAll(`[data-component="${selectedComponent}"]`).forEach(innerEl => {
-        applyHighlight(innerEl);
-      });
-    }
-  });
+      if (el.shadowRoot) {
+        el.shadowRoot
+          .querySelectorAll(`[data-component="${selectedComponent}"]`)
+          .forEach((innerEl) => {
+            applyHighlight(innerEl);
+          });
+      }
+    });
 };
 
 // Events
@@ -143,10 +160,13 @@ document.addEventListener("DOMContentLoaded", function () {
       {
         target: { tabId: tabs[0].id, allFrames: true },
         func: detectSystems,
-        args: [data]
+        args: [data],
       },
       function (response) {
-        if (response[0].result.systemDetections.length && response[0].result.DSComponentDetections.length) {
+        if (
+          response[0].result.systemDetections.length &&
+          response[0].result.DSComponentDetections.length
+        ) {
           buildPopUpTools(
             response[0].result.systemDetections,
             response[0].result.DSComponentDetections,
@@ -158,25 +178,31 @@ document.addEventListener("DOMContentLoaded", function () {
     );
   });
 
-  document.querySelector("#themes-select").addEventListener("change", function (event) {
-    const selectedValue = event.target.value;
-    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-      chrome.scripting.executeScript({
-        target: { tabId: tabs[0].id, allFrames: true },
-        func: changeTheme,
-        args: [selectedValue]
+  document
+    .querySelector("#themes-select")
+    .addEventListener("change", function (event) {
+      const selectedValue = event.target.value;
+      chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+        chrome.scripting.executeScript({
+          target: { tabId: tabs[0].id, allFrames: true },
+          func: changeTheme,
+          args: [selectedValue],
+        });
       });
     });
-  });
 
-  document.querySelector("#highlight-components-select").addEventListener("change", function (event) {
-    const selectedComponent = document.querySelector("#highlight-components-select").value;
-    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-      chrome.scripting.executeScript({
-        target: { tabId: tabs[0].id, allFrames: true },
-        func: highlightComponents,
-        args: [selectedComponent]
+  document
+    .querySelector("#highlight-components-select")
+    .addEventListener("change", function (event) {
+      const selectedComponent = document.querySelector(
+        "#highlight-components-select"
+      ).value;
+      chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+        chrome.scripting.executeScript({
+          target: { tabId: tabs[0].id, allFrames: true },
+          func: highlightComponents,
+          args: [selectedComponent],
+        });
       });
     });
-  });
 });
